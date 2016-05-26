d5fd8383370903e78b202a513aa22419a24c65b1
Adolphsa <497749990@qq.com>
2016-05-26 19:25:05 +0800

    2016/05/26  测试导航栏

---
+++ app/src/main/java/com/zividig/ziv/fragments/FirstFragment.java
@@ -0,0 +1,28 @@
+package com.zividig.ziv.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zividig.ziv.R;
+
+/**
+ * Created by linhonghong on 2015/8/11.
+ */
+public class FirstFragment extends Fragment {
+
+    public static FirstFragment instance() {
+        FirstFragment view = new FirstFragment();
+		return view;
+	}
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.first,null);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/zividig/ziv/fragments/SecondFragment.java
@@ -0,0 +1,28 @@
+package com.zividig.ziv.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zividig.ziv.R;
+
+/**
+ * Created by linhonghong on 2015/8/11.
+ */
+public class SecondFragment extends Fragment {
+
+    public static SecondFragment instance() {
+        SecondFragment view = new SecondFragment();
+        return view;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.second, null);
+        return view;
+    }
+}
