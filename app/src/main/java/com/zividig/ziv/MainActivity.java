044391a005220b7cc0f6996e16f3c84aa7d50583
Adolphsa <497749990@qq.com>
2016-05-26 19:23:16 +0800

    2016/05/26  测试导航栏

--- app/src/main/java/com/zividig/ziv/MainActivity.java
+++ app/src/main/java/com/zividig/ziv/MainActivity.java
@@ -1,14 +1,179 @@
 package com.zividig.ziv;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 
-public class MainActivity extends Activity {
+import com.lhh.apst.library.AdvancedPagerSlidingTabStrip;
+import com.zividig.ziv.fragments.FirstFragment;
+import com.zividig.ziv.fragments.FourthFragment;
+import com.zividig.ziv.fragments.SecondFragment;
+import com.zividig.ziv.fragments.ThirdFragment;
+
+public class MainActivity extends FragmentActivity implements ViewPager.OnPageChangeListener{
+
+    public AdvancedPagerSlidingTabStrip mAPSTS;
+    public APSTSViewPager mVP;
+
+    private static final int VIEW_FIRST 		= 0;
+    private static final int VIEW_SECOND	    = 1;
+    private static final int VIEW_THIRD       = 2;
+    private static final int VIEW_FOURTH    = 3;
+
+    private static final int VIEW_SIZE = 4;
+
+    private FirstFragment mFirstFragment = null;
+    private SecondFragment mSecondFragment = null;
+    private ThirdFragment mThirdFragment = null;
+    private FourthFragment mFourthFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+        init();
+    }
+
+    private void findViews(){
+        mAPSTS = (AdvancedPagerSlidingTabStrip)findViewById(R.id.tabs);
+        mVP = (APSTSViewPager)findViewById(R.id.vp_main);
+    }
+
+    private void init(){
+        mVP.setOffscreenPageLimit(VIEW_SIZE);
+        FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager());
+
+        mVP.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+
+        adapter.notifyDataSetChanged();
+        mAPSTS.setViewPager(mVP);
+        mAPSTS.setOnPageChangeListener(this);
+        mVP.setCurrentItem(VIEW_FIRST);
+        mAPSTS.showDot(VIEW_FIRST,"99+");
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    public class FragmentAdapter extends FragmentStatePagerAdapter implements AdvancedPagerSlidingTabStrip.IconTabProvider{
+
+        public FragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if(position >= 0 && position < VIEW_SIZE){
+                switch (position){
+                    case  VIEW_FIRST:
+                        if(null == mFirstFragment)
+                            mFirstFragment = FirstFragment.instance();
+                        return mFirstFragment;
+
+                    case VIEW_SECOND:
+                        if(null == mSecondFragment)
+                            mSecondFragment = SecondFragment.instance();
+                        return mSecondFragment;
+
+                    case VIEW_THIRD:
+                        if(null == mThirdFragment)
+                            mThirdFragment = ThirdFragment.instance();
+                        return mThirdFragment;
+
+                    case VIEW_FOURTH:
+                        if(null == mFourthFragment)
+                            mFourthFragment = FourthFragment.instance();
+                        return mFourthFragment;
+                    default:
+                        break;
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return VIEW_SIZE;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if(position >= 0 && position < VIEW_SIZE){
+                switch (position){
+                    case  VIEW_FIRST:
+                        return  "我的车";
+                    case  VIEW_SECOND:
+                        return  "消息";
+                    case  VIEW_THIRD:
+                        return  "设置";
+                    case  VIEW_FOURTH:
+                        return  "我";
+                    default:
+                        break;
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public Integer getPageIcon(int index) {
+            if(index >= 0 && index < VIEW_SIZE){
+                switch (index){
+                    case  VIEW_FIRST:
+                        return  R.mipmap.carinfo_default;
+                    case VIEW_SECOND:
+                        return  R.mipmap.realtimeimg_default;
+                    case VIEW_THIRD:
+                        return  R.mipmap.setting_default;
+                    case VIEW_FOURTH:
+                        return  R.mipmap.route_walk_icon;
+                    default:
+                        break;
+                }
+            }
+            return 0;
+        }
+
+        @Override
+        public Integer getPageSelectIcon(int index) {
+            if(index >= 0 && index < VIEW_SIZE){
+                switch (index){
+                    case  VIEW_FIRST:
+                        return  R.mipmap.carinfo;
+                    case VIEW_SECOND:
+                        return  R.mipmap.realtimeimg;
+                    case VIEW_THIRD:
+                        return  R.mipmap.setting;
+                    case VIEW_FOURTH:
+                        return  R.mipmap.route_walk_icon_slecter;
+                    default:
+                        break;
+                }
+            }
+            return 0;
+        }
+
+        @Override
+        public Rect getPageIconBounds(int position) {
+            return null;
+        }
     }
 }
