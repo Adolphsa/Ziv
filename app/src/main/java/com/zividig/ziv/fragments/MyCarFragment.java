fc0d05e057965cdd00c1fe7ab4c2649a8e0ec897
Adolphsa <497749990@qq.com>
2016-05-27 16:38:35 +0800

    2016/05/27  广告循环播放（完成）

--- app/src/main/java/com/zividig/ziv/fragments/MyCarFragment.java
+++ app/src/main/java/com/zividig/ziv/fragments/MyCarFragment.java
@@ -1,22 +1,21 @@
 package com.zividig.ziv.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bigkoo.convenientbanner.ConvenientBanner;
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
+import com.bigkoo.convenientbanner.holder.Holder;
 import com.zividig.ziv.R;
-import com.zividig.ziv.adapter.ImagePagerAdapter;
-import com.zividig.ziv.utils.ListUtils;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
 
 /**
  * 我的车
@@ -24,53 +23,77 @@ import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
  */
 public class MyCarFragment extends Fragment {
 
-    private List<Integer>       imageIdList;
+
+    private ConvenientBanner convenientBanner; //顶部广告栏控件
+    private ArrayList<Integer> localImages = new ArrayList<Integer>();
+    private View view;
 
     public static MyCarFragment instance() {
-        MyCarFragment view = new MyCarFragment();
-		return view;
+        MyCarFragment myCarView = new MyCarFragment();
+		return myCarView;
 	}
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_mycar,null);
+        view = inflater.inflate(R.layout.fragment_mycar,null);
 
         //设置标题
-        TextView title = (TextView)view.findViewById(R.id.tv_title);
+        TextView title = (TextView) view.findViewById(R.id.tv_title);
         title.setText("我的车");
 
-        AutoScrollViewPager autoScrollViewPager = (AutoScrollViewPager) view.findViewById(R.id.auto_scroll_view_pager);
-        imageIdList = new ArrayList<Integer>();
-        imageIdList.add(R.mipmap.ad1);
-        imageIdList.add(R.mipmap.ad2);
-        imageIdList.add(R.mipmap.ad3);
-
-
-        autoScrollViewPager.setAdapter(new ImagePagerAdapter(getContext(), imageIdList).setInfiniteLoop(true));
-        autoScrollViewPager.addOnPageChangeListener(new MyOnChangeListener());
-        autoScrollViewPager.setInterval(2000);
-        autoScrollViewPager.startAutoScroll();
-        autoScrollViewPager.setCurrentItem(Integer.MAX_VALUE / 2 - Integer.MAX_VALUE / 2 % ListUtils.getSize(imageIdList));
-
+        initAd();
         return view;
     }
 
-    class MyOnChangeListener implements ViewPager.OnPageChangeListener{
+    //初始化广告控件
+    private void initAd(){
+        convenientBanner = (ConvenientBanner) view.findViewById(R.id.convenientBanner);
+        localImages = new ArrayList<>();
+        localImages.add(R.mipmap.ad1);
+        localImages.add(R.mipmap.ad2);
+        localImages.add(R.mipmap.ad3);
+
+        convenientBanner.setPages(new CBViewHolderCreator() {
+            @Override
+            public LocalImageHolderView createHolder() {
+                return new LocalImageHolderView();
+            }
+        },localImages)
+                //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
+                .setPageIndicator(new int[]{R.mipmap.ic_page_indicator, R.mipmap.ic_page_indicator_focused})
+                //设置指示器的方向
+                .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT);
+    }
+
+    public class LocalImageHolderView implements Holder<Integer> {
+        private ImageView imageView;
 
         @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+        public View createView(Context context) {
+            imageView = new ImageView(context);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            return imageView;
         }
 
         @Override
-        public void onPageSelected(int position) {
-
+        public void UpdateUI(Context context, final int position, Integer data) {
+            imageView.setImageResource(data);
         }
 
-        @Override
-        public void onPageScrollStateChanged(int state) {
+    }
 
-        }
+    @Override
+    public void onResume() {
+        super.onResume();
+        //开始自动翻页
+        convenientBanner.startTurning(3000);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //停止翻页
+        convenientBanner.stopTurning();
     }
 }
