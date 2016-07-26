6cd46f1b237749bb6340be61c513c330063ecbb7
Adolphsa <497749990@qq.com>
2016-07-26 17:07:38 +0800

    2016/07/26  修改登录和找回密码界面

--- app/src/main/java/com/zividig/ziv/main/FindPassWord.java
+++ app/src/main/java/com/zividig/ziv/main/FindPassWord.java
@@ -2,30 +2,30 @@ package com.zividig.ziv.main;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.text.InputType;
+import android.text.TextUtils;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.zividig.ziv.R;
 
 public class FindPassWord extends Activity {
 
-    private ListView listView;
+    private EditText etUser;
+    private EditText etPwd;
+    private EditText etYzm;
+
+    private String fdUser;
+    private String fdPwd;
+    private String fdYzm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_pass_word);
 
-        initView();
-    }
-
-    private void initView(){
         // 标题
         TextView txtTitle = (TextView) findViewById(R.id.tv_title);
         txtTitle.setText("重置密码");
@@ -40,69 +40,24 @@ public class FindPassWord extends Activity {
             }
         });
 
-        listView = (ListView) findViewById(R.id.finPwd_lv);
-        FindPwdListViewAdapter adapter = new FindPwdListViewAdapter();
-        listView.setAdapter(adapter);
+        initView();
+    }
+
+    private void initView(){
+
+        etUser = (EditText) findViewById(R.id.findPwd_et_user);
+        etPwd = (EditText) findViewById(R.id.findPwd_et_pwd);
+        etYzm = (EditText) findViewById(R.id.findPwd_et_yzm);
 
     }
 
-    class FindPwdListViewAdapter extends BaseAdapter {
-
-        ViewHolder holder;
-
-        @Override
-        public int getCount() {
-            return 4;
+    public void getFindPwdYzm(View view){
+        fdUser = etUser.getText().toString().trim();
+        fdPwd = etPwd.getText().toString().trim();
+        fdYzm = etYzm.getText().toString().trim();
+        if (TextUtils.isEmpty(fdUser)){
+            System.out.println(fdUser + "-----");
+            Toast.makeText(FindPassWord.this,"请先输入手机号码",Toast.LENGTH_SHORT).show();
         }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null){
-                convertView = View.inflate(FindPassWord.this,R.layout.layout_register_item,null);
-                holder = new ViewHolder();
-                holder.itemTv = (TextView) convertView.findViewById(R.id.register_item_tv);
-                holder.itemEt = (EditText) convertView.findViewById(R.id.register_item_et);
-                convertView.setTag(holder);
-            }else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            switch (position){
-                case 0:
-                    holder.itemTv.setText("用户名        ");
-                    holder.itemEt.setHint("请输入手机号码");
-                    break;
-                case 1:
-                    holder.itemTv.setText("设备ID号    ");
-                    holder.itemEt.setHint("请输入设备ID号");
-                    holder.itemEt.setInputType(InputType.TYPE_CLASS_NUMBER);
-                    break;
-                case 2:
-                    holder.itemTv.setText("密码            ");
-                    holder.itemEt.setHint("请输入新密码");
-                    break;
-                case 3:
-                    holder.itemTv.setText("确定密码    ");
-                    holder.itemEt.setHint("请再次确认新密码");
-                    break;
-
-            }
-            return convertView;
-        }
-    }
-
-    static class ViewHolder{
-        TextView itemTv;
-        EditText itemEt;
     }
 }
--- app/src/main/java/com/zividig/ziv/main/Register.java
+++ app/src/main/java/com/zividig/ziv/main/Register.java
@@ -2,30 +2,30 @@ package com.zividig.ziv.main;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.text.InputType;
+import android.text.TextUtils;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.zividig.ziv.R;
 
 public class Register extends Activity {
 
-    private ListView listView;
+    private EditText etUser;
+    private EditText etPwd;
+    private EditText etYzm;
+
+    private String user;
+    private String pwd;
+    private String yzm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        initView();
-    }
-
-    private void initView(){
         // 标题
         TextView txtTitle = (TextView) findViewById(R.id.tv_title);
         txtTitle.setText("注册");
@@ -40,68 +40,25 @@ public class Register extends Activity {
             }
         });
 
-        listView = (ListView) findViewById(R.id.register_lv);
-        RegisterListViewAdapter adapter = new RegisterListViewAdapter();
-        listView.setAdapter(adapter);
+        initView();
     }
 
-    class RegisterListViewAdapter extends BaseAdapter{
+    private void initView(){
+        etUser = (EditText) findViewById(R.id.register_et_user);
+        etPwd = (EditText) findViewById(R.id.register_et_pwd);
+        etYzm = (EditText) findViewById(R.id.register_et_yzm);
+    }
 
-        ViewHolder holder;
 
-        @Override
-        public int getCount() {
-            return 4;
-        }
 
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null){
-                convertView = View.inflate(Register.this,R.layout.layout_register_item,null);
-                holder = new ViewHolder();
-                holder.itemTv = (TextView) convertView.findViewById(R.id.register_item_tv);
-                holder.itemEt = (EditText) convertView.findViewById(R.id.register_item_et);
-                convertView.setTag(holder);
-            }else {
-                holder = (ViewHolder) convertView.getTag();
+    //获取验证码
+    public void getRegisterYzm(View view){
+        user = etUser.getText().toString().trim();
+        pwd = etPwd.getText().toString().trim();
+        yzm = etYzm.getText().toString().trim();
+            if (TextUtils.isEmpty(user)){
+                Toast.makeText(Register.this,"请先输入手机号码",Toast.LENGTH_SHORT).show();
             }
-
-            switch (position){
-                case 0:
-                    holder.itemTv.setText("用户名        ");
-                    holder.itemEt.setHint("请输入手机号码");
-                    holder.itemEt.setInputType(InputType.TYPE_CLASS_NUMBER);
-                    break;
-                case 1:
-                    holder.itemTv.setText("密码            ");
-                    holder.itemEt.setHint("请输入密码");
-                    break;
-                case 2:
-                    holder.itemTv.setText("确定密码    ");
-                    holder.itemEt.setHint("请再次确认密码");
-                    break;
-                case 3:
-                    holder.itemTv.setText("验证码");
-                    holder.itemEt.setHint("请输入验证码");
-                    holder.itemEt.setInputType(InputType.TYPE_CLASS_NUMBER);
-                    break;
-            }
-            return convertView;
-        }
     }
 
-    static class ViewHolder{
-        TextView itemTv;
-        EditText itemEt;
-    }
 }
