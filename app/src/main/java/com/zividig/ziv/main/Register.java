89618f8b512348a9bf2fa4a9693d9ef5a6cf7365
Adolphsa <497749990@qq.com>
2016-07-28 10:13:11 +0800

    2016/07/28  注册账号完成

--- app/src/main/java/com/zividig/ziv/main/Register.java
+++ app/src/main/java/com/zividig/ziv/main/Register.java
@@ -1,6 +1,7 @@
 package com.zividig.ziv.main;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -12,8 +13,11 @@ import android.widget.Toast;
 import com.google.gson.Gson;
 import com.zividig.ziv.R;
 import com.zividig.ziv.bean.RegisterMd5Bean;
+import com.zividig.ziv.customView.CountDownTimer;
 import com.zividig.ziv.utils.MD5;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.xutils.common.Callback;
 import org.xutils.http.RequestParams;
 import org.xutils.x;
@@ -32,6 +36,21 @@ public class Register extends Activity {
     private String yzm;
     private String md5Yzm;
     private RegisterMd5Bean registerMd5Bean;
+
+    private Button getYzmButton;
+
+    CountDownTimer countDownTimer = new CountDownTimer(60000,1000) {  //按钮倒计时
+        @Override
+        public void onTick(long millisUntilFinished) {
+            getYzmButton.setText(millisUntilFinished/1000 + "秒");
+        }
+
+        @Override
+        public void onFinish() {
+            getYzmButton.setEnabled(true);
+            getYzmButton.setText("获取验证码");
+        }
+    };
 
 
     @Override
@@ -57,6 +76,7 @@ public class Register extends Activity {
     }
 
     private void initView(){
+        getYzmButton = (Button) findViewById(R.id.register_item_bt);
         etUser = (EditText) findViewById(R.id.register_et_user);
         etPwd = (EditText) findViewById(R.id.register_et_pwd);
         etYzm = (EditText) findViewById(R.id.register_et_yzm);
@@ -67,7 +87,7 @@ public class Register extends Activity {
         user = etUser.getText().toString().trim();
         pwd = etPwd.getText().toString().trim();
         yzm = etYzm.getText().toString().trim();
-        System.out.println(user + "/n" + pwd + "/n" + yzm);
+        System.out.println(user + "\n" + pwd + "\n" + yzm);
         if (TextUtils.isEmpty(user) || user.length() != 11){
             System.out.println("usr的长度" + user.length());
             Toast.makeText(Register.this,"请输入手机号码",Toast.LENGTH_SHORT).show();
@@ -106,7 +126,8 @@ public class Register extends Activity {
 
                 @Override
                 public void onFinished() {
-
+                    getYzmButton.setEnabled(false);
+                    countDownTimer.start();
                 }
             });
         }
@@ -125,10 +146,61 @@ public class Register extends Activity {
 
             try {
                 System.out.println("md5Yzm:" + md5Yzm + "\n" + MD5.getMD5(tempYzm));
-                if (md5Yzm.equals(MD5.getMD5(tempYzm))){
+                if (md5Yzm.equals(MD5.getMD5(tempYzm))){  //判断MD5值是否相等
                     System.out.println("相等");
+                    System.out.println("user---" + user + ",pwd---" + pwd);
+                    JSONObject json = new JSONObject();
+                    try {
+                        json.put("username",user);
+                        json.put("password",pwd);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    RequestParams params = new RequestParams(REGISTER_URL);
+                    params.setAsJsonContent(true);
+                    params.setBodyContent(json.toString());
+
+                    System.out.println(params.toString());
+                    x.http().post(params, new Callback.CommonCallback<String>() {
+
+                        @Override
+                        public void onSuccess(String result) {
+                            System.out.println("提交成功---" + result);
+                            try {
+                                JSONObject json = new JSONObject(result);
+                                boolean issuccess = json.getBoolean("issuccess");
+                                if (issuccess){  //注册成功
+                                    Toast.makeText(Register.this,"注册成功",Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(Register.this,Login.class));
+                                    finish();
+
+                                }else {
+                                    Toast.makeText(Register.this,"注册失败",Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onError(Throwable ex, boolean isOnCallback) {
+                            System.out.println("注册错误");
+                        }
+
+                        @Override
+                        public void onCancelled(CancelledException cex) {
+
+                        }
+
+                        @Override
+                        public void onFinished() {
+
+                        }
+                    });
+
                 }else {
                     System.out.println("不相等");
+                    Toast.makeText(Register.this,"验证码不正确，请重新输入",Toast.LENGTH_SHORT).show();
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -136,41 +208,11 @@ public class Register extends Activity {
 
         }
 
-
-//        System.out.println("user---" + user + ",pwd---" + pwd);
-//        JSONObject json = new JSONObject();
-//        try {
-//            json.put("username",user);
-//            json.put("password",pwd);
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-//        RequestParams params = new RequestParams(REGISTER_URL);
-//        params.setAsJsonContent(true);
-//        params.setBodyContent(json.toString());
-//
-//        System.out.println(params.toString());
-//        x.http().post(params, new Callback.CommonCallback<String>() {
-//            @Override
-//            public void onSuccess(String result) {
-//                System.out.println("提交---" + result);
-//            }
-//
-//            @Override
-//            public void onError(Throwable ex, boolean isOnCallback) {
-//                System.out.println("注册错误");
-//            }
-//
-//            @Override
-//            public void onCancelled(CancelledException cex) {
-//
-//            }
-//
-//            @Override
-//            public void onFinished() {
-//
-//            }
-//        });
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        countDownTimer.cancel();
+    }
 }
